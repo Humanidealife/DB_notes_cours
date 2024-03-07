df437ab181f4b2ca0975dce7fc713eb3c684b56a
qianwang <wangqianlf@live.com>
2024-03-07 13:24:05 +0100

    initialisation du projet de notes en console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_notes_cours
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ReadKey();
+    }
+  }
+}
