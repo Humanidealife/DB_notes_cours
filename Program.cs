9453cb6a0f1d1eac9ffed4c662690b5e9f9a8801
qianwang <wangqianlf@live.com>
2024-03-07 14:26:04 +0100

    changement d'emplaçement

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace DB_notes_cours
   {
     static void Main(string[] args)
     {
-      using (StreamWriter sw = new StreamWriter("f:\\WANG\\Informatique\\Skilleos\\Cours\\Développer des applications en C#\\test.txt"))// si l'on ne précise pas le chemin, cela va créer un fichier dans le dossier du projet
+      using (StreamWriter sw = new StreamWriter("test.txt"))// si l'on ne précise pas le chemin, cela va créer un fichier dans le dossier du projet
       {
         sw.WriteLine("Mon premier texte.");
       }
